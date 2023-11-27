297494f455e470d0757772c87c12ccea4b68f142
Wh1teL0rd <pacman.oleg@gmail.com>
2023-11-27 10:40:13 +0200

    minor fixes

--- react-lab/src/services/apiService.js
+++ react-lab/src/services/apiService.js
@@ -1,30 +1,31 @@
 import axios from "axios";
-import {BASE_URL, FILTER_URL} from "../constants/apiUrls";
+import { BASE_URL, FILTER_URL } from "../constants/apiUrls";
 
-function generateFilterUrl({price, pages, author}) {
-    let url = '';
-    url += price !== '' ? `?price=${price}` : '?';
-    url += pages !== '' ? `&pages=${pages}` : '';
-    url += author !== '' ? `&author=${author}` : '';
-    return url;
+function generateFilterUrl({ price, pages, author }) {
+  const params = {};
+  if (price !== '') params.price = price;
+  if (pages !== '') params.pages = pages;
+  if (author !== '') params.author = author;
+
+  return params;
 }
 
 export async function getAllBooksRequest() {
-    try {
-        const response = await axios.get(BASE_URL);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const response = await axios.get(BASE_URL);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-export async function getFilteredBooks({price, pages, author}) {
-    const url = generateFilterUrl({price, pages, author})
-    console.log(FILTER_URL+url)
-    try {
-        const response = await axios.get(FILTER_URL + url)
-        return response.data;
-    } catch (error) {
-        console.error(error)
-    }
+export async function getFilteredBooks({ price, pages, author }) {
+  const params = generateFilterUrl({ price, pages, author });
+
+  try {
+    const response = await axios.get(FILTER_URL, { params });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
